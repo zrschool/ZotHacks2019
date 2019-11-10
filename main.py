71b44e7c745f51135543bdfd4d30ecb85544560a
Sylvester Yue <sylvesteryue2301@gmail.com>
2019-11-10 15:27:46 -0800

    modified main.py

--- main.py
+++ main.py
@@ -79,13 +79,13 @@ class AddHousingOptionPage(webapp2.RequestHandler):
 class HousingPage(webapp2.RequestHandler):
     def get(self):
         housing_id = self.request.get("id")
-        housing_query = list(HousingOption.query().fetch())
+        housing_query = housing.housing_option_list()
         current_housing = HousingOption(
             name = "dsa",
             rating = 0.0
         )
         for option in housing_query:
-            if get_key_id(option) == int(housing_id):
+            if housing.get_id(option) == int(housing_id):
                 current_housing = option
 
 
