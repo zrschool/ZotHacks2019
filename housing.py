2ae20746f1cd8c8966893f6d463f82961b899ee4
zacharyrideaux <zacharyrideaux@gmail.com>
2019-11-10 11:36:55 -0800

    Comment out unnecessary code (feel free to reimplement)

--- housing.py
+++ housing.py
@@ -1,31 +1,31 @@
 from google.appengine.ext import ndb
 
 class HousingOption(ndb.Model):
-    housing_name = ndb.StringProperty()
+    name = ndb.StringProperty()
     rating = ndb.FloatProperty()
-    description = ndb.StringProperty()
+    # description = ndb.StringProperty()
 
-def create_housing_option(name, rating):
+def create_housing_option(option_name, option_rating):
     new_option = HousingOption(
-        name = name,
-        rating = rating,
+        name = option_name,
+        rating = option_rating,
         # description = description
     )
     new_option.put()
-    return new_option.key()
+    # return new_option.key()
 
-def get_id(housing_option_key):
-    pair = housing_option_key.pairs()
-    return (housing_option_key.urlsafe(), pair[0][1])
-
-def get_entry(housing_option_key):
-    return housing_option_key.get()
-
-def get_name(housing_option):
-    return housing_option.name
-
-def get_rating(housing_option):
-    return housing_option.rating
-
-def get_description(housing_option):
-    return housing_option.description
+# def get_id(housing_option_key):
+#     pair = housing_option_key.pairs()
+#     return (housing_option_key.urlsafe(), pair[0][1])
+#
+# def get_entry(housing_option_key):
+#     return housing_option_key.get()
+#
+# def get_name(housing_option):
+#     return housing_option.name
+#
+# def get_rating(housing_option):
+#     return housing_option.rating
+#
+# def get_description(housing_option):
+#     return housing_option.description
