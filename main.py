488364080acc127209c67c962f713f3403b508d3
zacharyrideaux <zacharyrideaux@gmail.com>
2019-11-10 08:52:52 -0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import webapp2
+import jinja2
+import os
+
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+
+
+        template_vars = {
+            # "var_name" : var_name,
+        }
+
+        template = jinja_env.get_template("templates/landing.html")
+        self.response.write(template.render(template_vars))
+
+    def post(self):
+        pass
+
+
+app = webapp2.WSGIApplication([
+    ("/", MainPage),
+], debug=True)
